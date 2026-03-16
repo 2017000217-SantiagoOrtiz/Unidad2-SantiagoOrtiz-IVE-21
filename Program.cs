525198e4dc14179bd9be65ac179e2bf1061d76ec
2017000217-SantiagoOrtiz <s.ortiz.5bdb@gmail.com>
2026-03-16 12:20:38 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Numeros Negativos y Positivos");
+        int num;
+        Console.WriteLine("Ingrese un numero: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num > 0)
+        {
+            Console.WriteLine("El numero es positivo");
+        }
+        else
+        {
+                Console.WriteLine("El numero es negativo");
+
+        }
+    }
+}
