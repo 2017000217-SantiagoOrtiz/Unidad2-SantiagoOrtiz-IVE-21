aa86c02567c9303a9ce830065f894878835810d6
2017000217-SantiagoOrtiz <s.ortiz.5bdb@gmail.com>
2026-03-23 12:16:11 -0600

    Add files via upload
    Esto es una prueba

--- Program.cs
+++ Program.cs
@@ -2,23 +2,18 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("PROMEDIO DE NOTAS");
-        int n1, n2, n3, n4, n5;
-        string materia;
-        Console.WriteLine("Dijite la MAteria");
-        materia = Console.ReadLine();
-        Console.WriteLine("Dijite 5 notas:");
-        n1 = Convert.ToInt32(Console.ReadLine());
-        n2 = Convert.ToInt32(Console.ReadLine());
-        n3 = Convert.ToInt32(Console.ReadLine());
-        n4 = Convert.ToInt32(Console.ReadLine());
-        n5 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite su nota");
+        int nota = Convert.ToInt32(Console.ReadLine());
 
-        double promedio = (n1 + n2 + n3 + n4 + n5) / 5;
-        Console.WriteLine("La materia: " + materia);
-        Console.WriteLine("El Promedio es de:" + promedio);
-        if (promedio >= 70)
-            Console.WriteLine("Estudiante Aprobado");
-        else Console.WriteLine("Estudiante Reprobado");
+        if (nota >= 0 && nota < 60)
+            Console.WriteLine("Deficiente");
+        if (nota >= 60 && nota < 70)
+            Console.WriteLine("Regular");
+        if (nota >= 70 && nota < 80)
+            Console.WriteLine("Bien");
+        if (nota >= 80 && nota < 90)
+            Console.WriteLine("Muy Bien");
+        if (nota >= 90 && nota < 100)
+            Console.WriteLine("Excelente");
     }
 }
