992154daaed7394f982de40ab63235ca4167c5d5
2017000217-SantiagoOrtiz <s.ortiz.5bdb@gmail.com>
2026-03-23 11:37:59 -0600

    Add files via upload
    Ejemplo if en Cascada

--- Program.cs
+++ Program.cs
@@ -2,18 +2,36 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Numeros Negativos y Positivos");
-        int num;
-        Console.WriteLine("Ingrese un numero: ");
-        num = Convert.ToInt32(Console.ReadLine());
-        if (num > 0)
-        {
-            Console.WriteLine("El numero es positivo");
-        }
-        else
-        {
-                Console.WriteLine("El numero es negativo");
+        Console.WriteLine("MENSAJE DE VOZ");
+        int opc;
+        Console.Write("Menu principal\n");
+        Console.Write("1- Recepcion\n");
+        Console.Write("2- Vigilancia\n");
+        Console.Write("3- Mantenimiento\n");
+        Console.Write("4- Salir\n");
+        Console.Write("Digite una opcion []:");
+        opc = Convert.ToInt32(Console.ReadLine());
 
+        if (opc == 1)
+        {
+            Console.Clear();
+            Console.WriteLine("Bienvenido al Dep. de Recepcion");
         }
+        if (opc == 2)
+        {
+            Console.Clear();
+            Console.WriteLine("Bienvenido al Dep. de Vigilancia");
+        }
+        if (opc == 3)
+        {
+            Console.Clear();
+            Console.WriteLine("Bienvenido al Dep. de Mantenimiento");
+        }
+        if (opc != 1 && opc != 2 && opc != 3)
+        {
+            Console.Clear();
+            Console.WriteLine("Saliendo del sistema");
+        }
+
     }
 }
