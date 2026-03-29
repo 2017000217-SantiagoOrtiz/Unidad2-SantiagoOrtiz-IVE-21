0b67c2b051ee4296baf9da19d0d0f24f8c555d7e
2017000217-SantiagoOrtiz <s.ortiz.5bdb@gmail.com>
2026-03-28 18:53:27 -0600

    Add files via upload
    Problemas en C#

---
+++ C#/Act2_Lecc20_Inc1.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese su usuario y su contraseña");
+        string u = Console.ReadLine();
+        string c = Console.ReadLine();
+        if (c == "Pasword123")
+        {
+            Console.WriteLine("Bienvenido");
+        }
+        else
+        {
+            Console.WriteLine("No se le permite el ingreso");
+        }
+    }
+}
